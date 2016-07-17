642ec9f236e28f94aef17ae8fc1c32f18bfffa57
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2016-07-17 09:43:59 +0000

    Sketch denormalize script

---
+++ findthebot/denormalize.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+
+from app import Tweet, Tuser, tuser_schema, db
+
+import json
+import os
+import sys
+
+def run(tweet_ids):
+    tweets = Tweet.query.filter(Tweet.id.in_(tweet_ids)).all()
+    for tweet in tweets:
+        tuser = Tuser.query.filter(Tuser.user_id == tweet.user_id).first()
+        tuser_json = tuser_schema.dump(tuser)[0]
+        tweet.user = json.dumps(tuser_json)
+        db.session.add(tweet)
+    db.session.commit()
+    print(".", end='')
+
+tweet_ids = []
+
+for line in sys.stdin:
+    tweet_ids.append(int(line))
+    if len(tweet_ids) == int(os.getenv('BATCH_SIZE')):
+        run(tweet_ids)
+        tweet_ids = []
+
+run(tweet_ids)
+print("")
