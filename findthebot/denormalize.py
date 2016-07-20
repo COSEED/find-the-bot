ae6a50806716914ea1a3da51fad8468a5ec7354d
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2016-07-20 11:22:37 +0800

    Add denormalize script used to source control

--- findthebot/denormalize.py
+++ findthebot/denormalize.py
@@ -1,24 +1,34 @@
-from app import Tweet, Tuser, tuser_schema, db
-
+import psycopg2
 import json
 import os
 import sys
 
-def run(tweet_ids):
-    tweets = Tweet.query.filter(Tweet.tweet_id.in_(tweet_ids)).all()
-    for tweet in tweets:
-        tuser = Tuser.query.filter(Tuser.user_id == tweet.user_id).first()
-        tuser_json = tuser_schema.dump(tuser)[0]
-        tweet.user = json.dumps(tuser_json)
-        db.session.add(tweet)
-    db.session.commit()
-
-tweet_ids = []
+conn = psycopg2.connect(os.getenv('DATABASE_URL'))
+cursor = conn.cursor()
 
 for line in sys.stdin:
-    tweet_ids.append(int(line))
-    if len(tweet_ids) == int(os.getenv('BATCH_SIZE')):
-        run(tweet_ids)
-        tweet_ids = []
+	tweet_id = int(line)
+	cursor.execute("SELECT user_id FROM tweet WHERE tweet_id = %s", (tweet_id,))
+	(user_id,) = cursor.fetchone()
+	cursor.execute("SELECT * FROM tuser WHERE user_id = %s LIMIT 1", (user_id,))
+	result = cursor.fetchone()
+	row = {
+		'id': result[0],
+		'timestamp': result[1],
+		'user_id': result[2],
+		'screen_name': result[3],
+		'full_name': result[4],
+		'bio': result[5],
+		'followers': result[6],
+		'following': result[7],
+		'total_tweets': result[8],
+		'interesting': result[9],
+		'location': result[10],
+		'website': result[11],
+		'profile_image_url': result[12],
+		'profile_banner_url': result[13],
+		'protected': result[14],
+	}
+	cursor.execute("UPDATE tweet SET userblob=%s WHERE tweet_id = %s", (json.dumps(row), tweet_id))
+	conn.commit()
 
-run(tweet_ids)
